3d41659fe58f4f512451076c98bfb06c8277ec5e
starwarship <2408205158@qq.com>
2021-02-03 18:54:56 +0800

    y

--- src/until/BaseUrl.js
+++ src/until/BaseUrl.js
@@ -1,5 +1,5 @@
 // const baseurl = "http://172.22.4.2:8089"
-const baseurl = "http://39.100.140.143:8080"
+const baseurl = "http://120.78.180.59:8089"
 
 //目前只有下载使用了该文件的baseurl，如果地址改变，要更改这里的地址
 export default baseurl
